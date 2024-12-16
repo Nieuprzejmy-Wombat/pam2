c8fad00e5f8fe16cf8a23aa2c5e66ba4e7a70324
Nieuprzejmy-Wombat <u58_makcza_waw@technischools.com>
2024-12-16 15:12:16 +0100

    done

--- app/src/main/java/com/example/pam2/MainActivity.kt
+++ app/src/main/java/com/example/pam2/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pam2.ui.theme.Pam2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pam2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(Modifier.padding(innerPadding)) {
+                        Header("Available weather icons:")
+                        Prognosis(
+                            listOf(
+                                R.drawable.ic_cloudy,
+                                R.drawable.ic_sunny_and_cloudy,
+                                R.drawable.ic_sunny,
+                                R.drawable.ic_moon,
+                                R.drawable.ic_moon_clouds
+                            )
+                        )
+                    }
+
                 }
             }
         }
@@ -31,17 +51,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Prognosis(icons: List<Int>, modifier: Modifier = Modifier) {
+    LazyRow(Modifier.background(Color(0xff691996))) {
+        items(icons) {
+            Image(
+                painterResource(it),
+                contentDescription = "weather icon",
+                modifier = modifier
+                    .padding(15.dp, 10.dp)
+                    .size(80.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Pam2Theme {
-        Greeting("Android")
+fun Header(text: String, modifier: Modifier = Modifier) = Text(
+    text,
+    fontWeight = FontWeight.Bold,
+    fontSize = 45.sp,
+    lineHeight = 50.sp,
+    modifier = modifier.padding(15.dp)
+)
+
+@Preview
+@Composable
+fun Preview(){
+    Column {
+        Header("Available weather icons:")
+        Prognosis(
+            listOf(
+                R.drawable.ic_cloudy,
+                R.drawable.ic_sunny_and_cloudy,
+                R.drawable.ic_sunny,
+                R.drawable.ic_moon,
+                R.drawable.ic_moon_clouds
+            )
+        )
     }
 }
